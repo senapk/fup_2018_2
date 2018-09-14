4855b9811a9e4c92159fc32ed1c31d3408149d11
David Sena Oliveira <sena.ufc@gmail.com>
2018-09-14 13:04:16 -0300

    adivinha

---
+++ s06e02_busca_binaria/Readme.md
@@ -0,0 +1,33 @@
+
+```
+Escolha e um numero entre 0 e 100. Digite 1 para continuar.
+$ 1
+# faz de conta que você escolheu 40
+Seu numero eh 50?
+1(seu numero eh menor?) 2(eu acertei?) 3(seu numero eh maior?)
+$ 1
+Seu numero eh 25? 
+1(seu numero eh menor?) 2(eu acertei?) 3(seu numero eh maior?)
+$ 3
+Seu numero eh 37? 
+1(seu numero eh menor) 2(acertou) 3(seu numero eh maior)
+$ 3
+Seu numero eh 39? 
+1(seu numero eh menor) 2(acertou) 3(seu numero eh maior)
+$ 1
+
+```
+
+```
+Com quantos chutes em média ele precisa acerta usando busca binária?
+Com quantos chutes em média ele acerta usando busca aleatória?
+
+Qual a chance de a pessoa perder no jogo russo do tapa na cara.
+    Tem 2 pessoas disputando.
+    - O primeiro anota um número no papel e esconde.
+    - O segundo chuta números e o primeiro diz maior ou menor.
+        - Não existem limite de quantidade de chutes
+        - Se o segundo acertar o número na mosca ele ganha
+        - Se o intervalo se reduzir até o ponto de só ter um número dentro, o primeiro ganha
+        - Exemplo: inferior: 356 e superior 367
+```
--- s06e02_busca_binaria/adivinha2.c
+++ s06e02_busca_binaria/adivinha2.c
@@ -2,25 +2,34 @@
 #include <stdbool.h>
 
 int main(){
-    int lim_sup = 1000;
+    int lim_sup = 100;
     int lim_inf = 0;
     int qtd_chutes = 0;
     int opcao = 0;
 
-    puts("Pense num numero no intervalo [0, 1000]");
+    printf("Pense num numero no intervalo aberto ]0, 100[ e digite Enter para continuar");
+    getchar();
     while(true){
+        if(lim_inf + 1 == lim_sup){
+            printf("Não existem numeros entre %d e %d\n", lim_inf, lim_sup);
+            break;
+        }
+        if(lim_inf + 2 == lim_sup){
+            printf("Seu número é %d\n", lim_inf + 1);
+            printf("Acertei em %d chutes\n", qtd_chutes);
+            break;
+        }
         qtd_chutes += 1;
         int chute = (lim_sup + lim_inf) / 2;
-        printf("Chuto %d. (1)acertei? (2)Eh menor? (3) Eh maior: ", chute);
+        printf("%d: Chuto %2d entre ]%3d, %3d[.", qtd_chutes, chute, lim_inf, lim_sup);
+        printf("  (1)Acertei? Seu número eh (2)menor ou (3)maior?: ");
         scanf("%d", &opcao);
         if(opcao == 1)
             break;
         else if(opcao == 2)
-            lim_sup = chute - 1;
+            lim_sup = chute;
         else
-            lim_inf = chute + 1;
+            lim_inf = chute;
     }
-
-    printf("acertou em %d chutes", qtd_chutes);
     return 0;
 }
